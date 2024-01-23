c5380102406e6862280b42488fbda69a94ad05e5
mac <isaiasleite230@gmail.com>
2024-01-23 15:54:03 -0400

    publish/subscribe

--- Send/Send.cs
+++ Send/Send.cs
@@ -5,16 +5,11 @@ var factory = new ConnectionFactory { HostName = "host.docker.internal", UserNam
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "codap-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
 
 var message = GetMessage(args);
 var body = Encoding.UTF8.GetBytes(message);
-
-var properties = channel.CreateBasicProperties();
-properties.Persistent = true;
-
-channel.BasicPublish(exchange: "amq.direct", routingKey: "payment", basicProperties: null, body: body);
+channel.BasicPublish(exchange: "logs", routingKey: string.Empty, basicProperties: null, body: body);
 Console.WriteLine($" [x] Sent {message}");
 
 Console.WriteLine(" Press [enter] to exit.");
@@ -22,5 +17,5 @@ Console.ReadLine();
 
 static string GetMessage(string[] args)
 {
-  return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+  return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
 }
