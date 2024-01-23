68e343e7332b909506da948e5dca8ec5ebbc448c
mac <isaiasleite230@gmail.com>
2024-01-23 14:48:39 -0400

    work queues

--- Send/Send.cs
+++ Send/Send.cs
@@ -6,12 +6,21 @@ using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
 channel.QueueDeclare(queue: "codap-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
-var message = "Hello World! this is a message";
+var message = GetMessage(args);
 var body = Encoding.UTF8.GetBytes(message);
+
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
 
 channel.BasicPublish(exchange: "amq.direct", routingKey: "payment", basicProperties: null, body: body);
 Console.WriteLine($" [x] Sent {message}");
 
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+  return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
